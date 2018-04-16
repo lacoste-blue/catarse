482d3afed9797ea6b763691a17db2137fce07ca0
Lance Lacoste <llacoste@users.noreply.github.com>
2018-04-16 18:58:50 -0500

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
+require 'simplecov'
+require 'simplecov-json'
+SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::JSONFormatter,
+])
+SimpleCov.start
 
 RSpec.configure do |config|
   require 'zonebie'
